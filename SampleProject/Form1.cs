6cfd780559c902993ab25b1a31c59572aaa95f5d
y.toba <yushi.toba@ge-creative.co.jp>
2022-11-13 12:31:16 +0900

    Revert "ログ出力機能を追加しました。"
    This reverts commit 1e7ea5b74b6aee7957985b3b1540ec937488be0a.

--- SampleProject/Form1.cs
+++ SampleProject/Form1.cs
@@ -12,31 +12,9 @@ namespace SampleProject
 {
     public partial class Form1 : Form
     {
-        /// <summary>
-        /// コンストラクタ
-        /// </summary>
         public Form1()
         {
             InitializeComponent();
         }
-
-        /// <summary>
-        ///　ロードイベント
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            OutputConsoleLog("aaa");
-        }
-
-        /// <summary>
-        /// コンソール出力
-        /// </summary>
-        private void OutputConsoleLog(string text)
-        {
-            Console.WriteLine(text);
-        }
-
     }
 }
